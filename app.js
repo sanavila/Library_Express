f4b9b17005d6202ac62390322601725e6efcb5cc
sanavila <rute.leone@hotmail.com>
2023-01-03 09:35:08 -0300

    creating new route using Router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var wiki = require('./routes/wiki');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/wiki", wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
